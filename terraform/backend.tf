b552a935792ec466b7b475e4efadc13c9a1263c3
mateuszkobylski95 <mateusz.kobylski1995@gmail.com>
2026-06-14 11:28:03 +0200

    Configure terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstate9f2f5afa"
+    storage_account_name = "tfstatee1c19c4c"
     container_name       = "tfstate"
-    key                  = "validationtest.tfstate"
+    key                  = "testazure.tfstate"
   }
 }
